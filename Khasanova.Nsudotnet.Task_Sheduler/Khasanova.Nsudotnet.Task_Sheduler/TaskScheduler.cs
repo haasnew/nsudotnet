f8c282c80316e7dff2e41e4de3d155b21984d4e0
Асель Хасанова <asel_khas@list.ru>
2015-05-25 01:50:44 +0600

    new

---
+++ Khasanova.Nsudotnet.Task_Sheduler/Khasanova.Nsudotnet.Task_Sheduler/TaskScheduler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TaskScheduler
+{
+    class Program
+    {
+        static void Main()
+        {
+            var ts = new TaskScheduler();
+            Console.WriteLine(DateTime.Now);
+
+            try
+            {
+                ts.SchedulePeriodicJob(() => Console.WriteLine("{0}, Выполнение началось!", DateTime.Now), "* * * * *");
+                
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
